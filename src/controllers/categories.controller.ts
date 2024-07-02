253153559a008186121d52c48e203cb0cd0c2e38
ailtonjunior11 <ailtonjunior.dev.frontend@gmail.com>
2024-07-02 08:07:53 -0300

    Atualizações

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,22 +1,28 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 import { CategoriesService } from '../services/categories.service';
 import { CategoryModel } from '../database/schemas/category.schema';
 import { CategoriesRepository } from '../database/repositories/categories.repository';
 import { CreateCategoryDTO } from '../dtos/categories.dto';
+import { StatusCodes } from 'http-status-codes';
 
 export class CategoriesController {
   async create(
     req: Request<unknown, unknown, CreateCategoryDTO>,
     res: Response,
+    next: NextFunction,
   ) {
-    const { title, color } = req.body;
+    try {
+      const { title, color } = req.body;
 
-    const repository = new CategoriesRepository(CategoryModel);
-    const service = new CategoriesService(repository);
+      const repository = new CategoriesRepository(CategoryModel);
+      const service = new CategoriesService(repository);
 
-    const result = await service.create({ title, color });
+      const result = await service.create({ title, color });
 
-    return res.status(201).json(result);
+      return res.status(StatusCodes.CREATED).json(result);
+    } catch (err) {
+      next(err);
+    }
   }
 }
--- src/services/categories.service.ts
+++ src/services/categories.service.ts
@@ -1,11 +1,19 @@
+import { StatusCodes } from 'http-status-codes';
 import { CreateCategoryDTO } from '../dtos/categories.dto';
 import { Category } from '../entities/category.entity';
+import { AppError } from '../errors/app.error';
 import { CategoriesRepository } from './../database/repositories/categories.repository';
 
 export class CategoriesService {
   constructor(private CategoriesRepository: CategoriesRepository) {}
 
   async create({ title, color }: CreateCategoryDTO): Promise<Category> {
+    const foundCategory = await this.CategoriesRepository.findByTitle(title);
+
+    if (foundCategory) {
+      throw new AppError('Category already exists.', StatusCodes.BAD_REQUEST);
+    }
+
     const category = new Category({
       title,
       color,
